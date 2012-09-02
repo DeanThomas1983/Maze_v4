057a483a94dfa45ba8239f2328431c1054101400
Dean Thomas <dean.plymouth@googlemail.com>
2012-09-02 18:25:55 +0100

    more Javadoc commenting.

--- src/maze_v4/DataModel.java
+++ src/maze_v4/DataModel.java
@@ -19,16 +19,41 @@ public class DataModel
     IMazeStructure mazeStructure;
     MazeGenerator mazeGenerator;
 
+    /**
+     *  Accessor method for the MazeStructure
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return MazeStructure the structure used to hold the maze
+     *
+     */
     public IMazeStructure getMazeStructure()
     {
         return mazeStructure;
     }
 
+    /**
+     *  Accessor method for the MazeGenerator
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return MazeGenerator the maze generator used to schedule generation
+     *                        events
+     */
     public MazeGenerator getMazeGenerator()
     {
         return mazeGenerator;
     }
 
+    /**
+     *  As the class is a singleton any calls to the constructor are handled by
+     *  the method itself
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     */
     private DataModel()
     {
         mazeStructure = new SquareMazeStructure(5,5);
@@ -36,6 +61,16 @@ public class DataModel
 
     }
 
+    /**
+     *  If the class has already been instantiated return that instance of the
+     *  data model otherwise call the Private constructor to create a new
+     *  instance
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return DataModel - the data model
+     */
     public static DataModel getInstance()
     {
         if (uniqueInstance == null)
--- src/maze_v4/Wall.java
+++ src/maze_v4/Wall.java
@@ -16,21 +16,54 @@ public class Wall
     private IMazeCell connectedCell;
     private Boolean blocked;
 
+    /**
+     *  Accessor method to return the owner of the wall object
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return IMazeCell the cell which owns the wall
+     */
     public IMazeCell getOwner()
     {
         return owner;
     }
-    
+
+    /**
+     *  Accessor method to return if a wall is blocking or not
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return true if the wall is blocking
+     */
     public Boolean getBlocked()
     {
         return blocked;
     }
 
+    /**
+     *  Setter method to allow us to set if a wall is blocking or not
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @param blocked should be true if the wall is blocking
+     */
     public void setBlocked(Boolean blocked)
     {
         this.blocked = blocked;
     }
 
+    /**
+     *  Constructor
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @param owner the cell which will own the wall
+     *  @param connectedCell the cell which should reside behind the wall
+     */
     public Wall(IMazeCell owner,
                 IMazeCell connectedCell)
     {
@@ -39,11 +72,27 @@ public class Wall
         this.blocked = true;
     }
 
+    /**
+     *  Accessor method to return the cell behind the wall (if any)
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @return IMazeCell the cell behind the wall
+     */
     public IMazeCell getConnectedCell()
     {
         return this.connectedCell;
     }
-    
+
+    /**
+     *  Setter method to allow us to add a neighboring cell behind this wall
+     *
+     *  @author dean
+     *  @since 1-9-2012
+     *
+     *  @param mazeCell the cell to connect the wall to
+     */
     public void setConnectedCell(IMazeCell mazeCell)
     {
         this.connectedCell = mazeCell;
